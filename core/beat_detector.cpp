7fae7445d7383a2fd6b01176dd229324181f7a47
Tim Redfern <redfernt@gmail.com>
2026-06-19 00:42:14 +0100

    default to standard beat detection

--- core/beat_detector.cpp
+++ core/beat_detector.cpp
@@ -26,7 +26,7 @@ const EffectUILayout BeatDetector::ui_layout_ = {
             .text = "",
             .type = ControlType::RADIO_GROUP,
             .x = 4, .y = 11, .w = 49, .h = 20,
-            .default_val = 1,  // Advanced mode default
+            .default_val = 0,  // Standard mode default
             .radio_options = {
                 {"Standard", 0, 0, 45, 10},
                 {"Advanced", 0, 10, 49, 10}
